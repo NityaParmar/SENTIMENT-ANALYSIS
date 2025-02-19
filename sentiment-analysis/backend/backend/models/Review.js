6e2a4949994e06cfd8bc30d450f5053537821947
Nitya Parmar <lookmomicanfly017@gmail.com>
2025-02-19 21:22:59 +0530

    Delete sentiment-analysis/backend directory

--- sentiment-analysis/backend/backend/models/Review.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const reviewSchema = new mongoose.Schema({
-    text: String,
-    sentiment: String,  // Positive, Negative, Neutral
-    createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model("Review", reviewSchema);
